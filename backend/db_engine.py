a82fbe61405c52243dd4f2a016371087299296a2
Brandon Braner <brandon.braner@gmail.com>
2024-09-25 16:22:34 -0500

    setup settings and environment files

--- backend/db_engine.py
+++ backend/db_engine.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine
-
+from hellopatient.config import get_settings
 from models import Base
 
-_main_uri = "postgres:postgres@localhost:5432/postgres"
+settings = get_settings()
+
+_main_uri = f"""{settings.pg_username}:{settings.pg_password}@{settings.pg_host}:{settings.pg_port}/{settings.pg_database}"""
 _sync_uri = f"postgresql://{_main_uri}"
 _async_uri = f"postgresql+asyncpg://{_main_uri}"
 
