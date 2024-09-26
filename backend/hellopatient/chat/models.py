d9b0df0afa995823510cf7393b7aa8bee1fd0f9e
Brandon Braner <brandon.braner@gmail.com>
2024-09-25 19:04:33 -0500

    OpenAI with chat history working

--- backend/hellopatient/chat/models.py
+++ backend/hellopatient/chat/models.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 class ProductRecommendationRequest(BaseModel):
     user_input: str
+    session_id: str
 
 class ProductRecommendationResponse(BaseModel):
     recommendation: str
+    session_id: str
--- backend/hellopatient/llm_integrations/openai_integration.py
+++ backend/hellopatient/llm_integrations/openai_integration.py
@@ -1,24 +1,23 @@
 import os
-from langchain_core.messages import HumanMessage
-from langchain_core.runnables.history import RunnableWithMessageHistory
-
-from langchain_community.chat_message_histories import SQLChatMessageHistory
-
-
-
-os.environ["OPENAI_API_KEY"] = ""
 
 from langchain_openai import ChatOpenAI
 
-def open_ai_integration(model):
+from hellopatient.config import get_settings
+
+settings = get_settings()
+
+os.environ["OPENAI_API_KEY"] = f"{settings.openai_api_key}"
+
+
+
+def open_ai_integration(model: str):
     valid_models = ["gpt-4o-mini"]
+
     if model not in valid_models:
         raise ValueError(f"Invalid model. Choose from: {', '.join(valid_models)}")
+    
     return ChatOpenAI(
             model=model,
             temperature=0.0,
             max_tokens=1000
     )
-
-
-
